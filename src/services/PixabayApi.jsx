1669842fc9997ca73fab2d913de476188a3c6509
Andrii Mohylnytskyi <andrii.mogylnytskyi@gmail.com>
2023-01-27 18:14:51 +0100

    Bg fix

--- src/services/PixabayApi.jsx
+++ src/services/PixabayApi.jsx
@@ -3,8 +3,6 @@ import axios from "axios";
 const BASE_URL = "https://pixabay.com/api/";
 const KEY = "25390634-4c32e13f52f3d8663f768ede1";
 
-export const getImage = (query, page, perPage) => {
-  return axios.get(
-    `${BASE_URL}?key=${KEY}&q=${query}&page=${page}&per_page=${perPage}`
-  );
+export const getImage = (query, page) => {
+  return axios.get(`${BASE_URL}?key=${KEY}&q=${query}&page=${page}`);
 };
